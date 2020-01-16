35c4ffacc2e1ca0f08e1efe5377b24bf1df6e739
andrew-lewell <andrew.lewell@gmail.com>
2020-01-16 17:46:00 +0000

    added auth

--- stop-motion-api/app/controllers/films_controller.rb
+++ stop-motion-api/app/controllers/films_controller.rb
@@ -1,2 +1,27 @@
 class FilmsController < ApplicationController
+
+    before_action :protected_action
+
+    def create
+        film = Film.create(film_params)
+        film.update(user: @current_user)
+
+        if film.valid?
+            render json: film 
+        else 
+            render json: { errors: film.errors.full_messages }, status: :not_acceptable
+        end 
+    end 
+
+    private
+
+    def protected_action
+        if !logged_in?
+            render json: { errors: "you must be logged in" }, status: :unauthorized
+        end
+    end 
+
+    def film_params
+        params.require(:film).permit(:title, :description)
+    end
 end
