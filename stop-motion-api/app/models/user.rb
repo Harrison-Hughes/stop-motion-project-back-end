a51e5a7caab6954721f8227241230516d7ab4b8e
andrew-lewell <andrew.lewell@gmail.com>
2020-01-20 17:34:24 +0000

    finished user auth

--- stop-motion-api/app/models/user.rb
+++ stop-motion-api/app/models/user.rb
@@ -1,8 +1,8 @@
 class User < ApplicationRecord
     has_many :films
     has_secure_password
-    validates :username, :email, presence: true
-    validates :username, :email, uniqueness: { case_sensitive: false }
+    validates :username, presence: true
+    validates :username, uniqueness: { case_sensitive: false }
 
     def token
         JWT.encode({ user_id: self.id }, ENV['RAILS_SECRET'])
--- stop-motion-api/config/routes.rb
+++ stop-motion-api/config/routes.rb
@@ -2,7 +2,8 @@ Rails.application.routes.draw do
   resources :frames
   resources :films
   resources :users
-  post '/login', to: 'auth#create'
+  post '/login', to: 'users#login'
+  post '/validate', to: 'users#validate'
   
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
